890c6ab873e2a4a93bb8ace38df16803bf6585c7
Felipe dos Santos <ofelipe.dev@gmail.com>
2022-08-04 15:51:16 -0300

    refactor: youtubeUrlFormatter

--- src/assets/helpers/youtubeUrlFormatter.ts
+++ src/assets/helpers/youtubeUrlFormatter.ts
@@ -1,13 +1,25 @@
 export function youtubeUrlFormatter(url:string):string{
     const actualUrl = new URL(url)
-    const validateUrl = /^(https?\:\/\/)?(www\.youtube\.com|youtu\.be)\/.+$/
-    const baseUrl = 'https://www.youtube-nocookie.com/embed/'
+    const validateUrl = /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(-nocookie)?\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/
+    const baseUrl = 'https://www.youtube-nocookie.com/embed'
 
     if(validateUrl.test(actualUrl.href)){
-        const href = actualUrl.href.replace('watch?v=', '')
-        
-        const urlParams = href.split('/')
-        const id = urlParams[urlParams.length - 1]
+        const searchParams = actualUrl.searchParams
+        const pathName = actualUrl.pathname
+        let id:string
+
+        if(pathName.includes('embed')){
+            const paths = pathName.split('/')  
+            id = paths[paths.length - 1]
+            return `${baseUrl}/${id}`
+        }
+
+        if(searchParams.get('v')){  
+            id = searchParams.get('v')
+            return `${baseUrl}/${id}`
+        }
+
+        id = pathName
         return `${baseUrl}${id}`
     }
 }
