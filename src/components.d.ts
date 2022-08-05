9d96106e60d7802f680b25b56b113eab7fe2cd75
Felipe dos Santos <ofelipe.dev@gmail.com>
2022-08-04 22:10:00 -0300

    feat: composição componente ux-testimonials

--- src/components.d.ts
+++ src/components.d.ts
@@ -10,9 +10,19 @@ export namespace Components {
     interface UxBanners {
         "imagesList": string;
     }
+    interface UxTestimonials {
+        "description": string;
+        "testimonialsList": string;
+        "title": string;
+    }
     interface UxTextImage {
         "contentList": string;
         "side": sideType;
+    }
+    interface UxVideo {
+        "description": string;
+        "src": string;
+        "title": string;
     }
 }
 declare global {
@@ -22,28 +32,54 @@ declare global {
         prototype: HTMLUxBannersElement;
         new (): HTMLUxBannersElement;
     };
+    interface HTMLUxTestimonialsElement extends Components.UxTestimonials, HTMLStencilElement {
+    }
+    var HTMLUxTestimonialsElement: {
+        prototype: HTMLUxTestimonialsElement;
+        new (): HTMLUxTestimonialsElement;
+    };
     interface HTMLUxTextImageElement extends Components.UxTextImage, HTMLStencilElement {
     }
     var HTMLUxTextImageElement: {
         prototype: HTMLUxTextImageElement;
         new (): HTMLUxTextImageElement;
     };
+    interface HTMLUxVideoElement extends Components.UxVideo, HTMLStencilElement {
+    }
+    var HTMLUxVideoElement: {
+        prototype: HTMLUxVideoElement;
+        new (): HTMLUxVideoElement;
+    };
     interface HTMLElementTagNameMap {
         "ux-banners": HTMLUxBannersElement;
+        "ux-testimonials": HTMLUxTestimonialsElement;
         "ux-text-image": HTMLUxTextImageElement;
+        "ux-video": HTMLUxVideoElement;
     }
 }
 declare namespace LocalJSX {
     interface UxBanners {
         "imagesList"?: string;
     }
+    interface UxTestimonials {
+        "description"?: string;
+        "testimonialsList"?: string;
+        "title"?: string;
+    }
     interface UxTextImage {
         "contentList"?: string;
         "side"?: sideType;
     }
+    interface UxVideo {
+        "description"?: string;
+        "src"?: string;
+        "title"?: string;
+    }
     interface IntrinsicElements {
         "ux-banners": UxBanners;
+        "ux-testimonials": UxTestimonials;
         "ux-text-image": UxTextImage;
+        "ux-video": UxVideo;
     }
 }
 export { LocalJSX as JSX };
@@ -51,7 +87,9 @@ declare module "@stencil/core" {
     export namespace JSX {
         interface IntrinsicElements {
             "ux-banners": LocalJSX.UxBanners & JSXBase.HTMLAttributes<HTMLUxBannersElement>;
+            "ux-testimonials": LocalJSX.UxTestimonials & JSXBase.HTMLAttributes<HTMLUxTestimonialsElement>;
             "ux-text-image": LocalJSX.UxTextImage & JSXBase.HTMLAttributes<HTMLUxTextImageElement>;
+            "ux-video": LocalJSX.UxVideo & JSXBase.HTMLAttributes<HTMLUxVideoElement>;
         }
     }
 }
