a2b7ec2835fcbdbe20c7f0e61ca14359fd10bbc0
Felipe dos Santos <ofelipe.dev@gmail.com>
2022-08-04 21:36:40 -0300

    refactor: youtubeUrlFormatter

--- src/assets/helpers/youtubeUrlFormatter.ts
+++ src/assets/helpers/youtubeUrlFormatter.ts
@@ -1,25 +1,22 @@
+function youtubeUrlValidator(url:string):boolean{
+    const regexYoutubeUrl = /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(-nocookie)?\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/
+
+    return regexYoutubeUrl.test(url)
+}
+
+function youtubeIdUrl(url:string):string{
+    const currentUrl = new URL(url)
+    const searchParams = currentUrl.searchParams
+    const pathName = currentUrl.pathname
+
+    return searchParams.get('v') ?? pathName.substring(pathName.lastIndexOf('/') + 1)
+}
+
 export function youtubeUrlFormatter(url:string):string{
-    const actualUrl = new URL(url)
-    const validateUrl = /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(-nocookie)?\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/
+    if(!youtubeUrlValidator(url)) throw new Error('URL inválida. Insira uma URL que respeite os padrões do Youtube')
+
     const baseUrl = 'https://www.youtube-nocookie.com/embed'
+    const id = youtubeIdUrl(url)
 
-    if(validateUrl.test(actualUrl.href)){
-        const searchParams = actualUrl.searchParams
-        const pathName = actualUrl.pathname
-        let id:string
-
-        if(pathName.includes('embed')){
-            const paths = pathName.split('/')  
-            id = paths[paths.length - 1]
-            return `${baseUrl}/${id}`
-        }
-
-        if(searchParams.get('v')){  
-            id = searchParams.get('v')
-            return `${baseUrl}/${id}`
-        }
-
-        id = pathName
-        return `${baseUrl}${id}`
-    }
+    return `${baseUrl}/${id}`
 }
