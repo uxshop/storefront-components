c8d0a53bcd8766cc2faf45b94032e0c20634b2f0
vitorbruski <vitorgbruski@gmail.com>
2022-12-28 08:57:24 -0300

    fix: revisão de componente

---
+++ src/components/ux-video/readme.md
@@ -0,0 +1,19 @@
+# ux-video
+
+
+
+<!-- Auto Generated Below -->
+
+
+## Properties
+
+| Property          | Attribute          | Description | Type     | Default     |
+| ----------------- | ------------------ | ----------- | -------- | ----------- |
+| `dataDescription` | `data-description` |             | `string` | `undefined` |
+| `dataSrc`         | `data-src`         |             | `string` | `undefined` |
+| `dataTitle`       | `data-title`       |             | `string` | `undefined` |
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
--- src/components/ux-video/ux-video.tsx
+++ src/components/ux-video/ux-video.tsx
@@ -1,40 +1,33 @@
-import {Component, h, Prop, State} from '@stencil/core'
+import { Component, h, Prop, State } from '@stencil/core'
 import { youtubeUrlFormatter } from '../../assets/helpers/youtubeUrlFormatter'
 @Component({
-    tag: 'ux-video',
-    styleUrl: 'ux-video.scss'
+  tag: 'ux-video',
+  styleUrl: 'ux-video.scss'
 })
-
 export class UxVideo {
-    @State() video : string
+  @State() video: string
 
-    @Prop() dataTitle: string
-    @Prop() dataDescription: string
-    @Prop() dataSrc: string
+  @Prop() dataTitle: string
+  @Prop() dataDescription: string
+  @Prop() dataSrc: string
 
-    componentWillLoad(){
-        this.video = youtubeUrlFormatter(this.dataSrc)
-    }
+  componentWillLoad() {
+    this.video = youtubeUrlFormatter(this.dataSrc)
+  }
 
-    render(){
-        return(
-            <section class="ux-video">
-                <div class="content">
-                    <div class="infos">
-                        <h2 class="title">{this?.dataTitle}</h2>
-                        <p class="description">{this?.dataDescription}</p>
-                    </div>
-                    <div class="video-container">
-                        <iframe 
-                            src={this.video} 
-                            width="100%"
-                            height="100%"
-                            allowFullScreen
-                        >
-                        </iframe>
-                    </div>
-                </div>
-            </section> 
-        )
-    }
+  render() {
+    return (
+      <section class="ux-video">
+        <div class="content">
+          <div class="infos">
+            <h2 class="title">{this?.dataTitle}</h2>
+            <p class="description">{this?.dataDescription}</p>
+          </div>
+          <div class="video-container">
+            <iframe src={this.video} width="100%" height="100%" allowFullScreen />
+          </div>
+        </div>
+      </section>
+    )
+  }
 }
