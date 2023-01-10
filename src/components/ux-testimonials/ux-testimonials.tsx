5fb13e570d715bb0d7587f18017dff60488e4f18
LimaBagy08 <joao.lima@bagy.com.br>
2023-01-10 16:01:39 -0300

    refactor: Remove tag quando não possuir mensagem  e ajusta reatividade

--- src/components/ux-testimonials/ux-testimonials.tsx
+++ src/components/ux-testimonials/ux-testimonials.tsx
@@ -1,4 +1,4 @@
-import { Component, h, Prop, State } from '@stencil/core'
+import { Component, h, Prop, State, Watch } from '@stencil/core'
 import { testimonialsType } from './ux-testimonials-type'
 import icon from '../../assets/icons/testimonials-icon.svg'
 
@@ -17,6 +17,11 @@ export class UxTestimonials {
     this.testimonials = JSON.parse(this.dataTestimonials)
   }
 
+  @Watch('dataTestimonials')
+  watchPropHandler(newValue: string) {
+    this.testimonials = JSON.parse(newValue)
+  }
+
   render() {
     return (
       <section class="ux-testimonials">
@@ -25,19 +30,21 @@ export class UxTestimonials {
           <p class="description">{this?.dataDescription}</p>
         </div>
         <div class="testimonials-container">
-          {this.testimonials.map(testimonial => {
-            return (
-              <div class="testimonial">
-                <img src={icon} alt={null} class="icon" />
-                <p class="message">{testimonial?.message}</p>
+          {this.testimonials &&
+            this.testimonials.map(
+              testimonial =>
+                testimonial.message && (
+                  <div class="testimonial">
+                    <img src={icon} alt={null} class="icon" />
+                    <p class="message">{testimonial?.message}</p>
 
-                <div class="customer">
-                  <img src={testimonial?.icon} alt={null} class="customer-icon" />
-                  <h3 class="customer-name">{testimonial?.name}</h3>
-                </div>
-              </div>
-            )
-          })}
+                    <div class="customer">
+                      <img src={testimonial?.icon} alt={null} class="customer-icon" />
+                      <h3 class="customer-name">{testimonial?.name}</h3>
+                    </div>
+                  </div>
+                )
+            )}
         </div>
       </section>
     )
